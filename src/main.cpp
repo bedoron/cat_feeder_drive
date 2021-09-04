7f496535d47661ed4722af656e1edcce3a9aff32
B.E. Doron <b.e.doron@gmail.com>
2021-09-05 00:45:00 +0300

    started POC

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
 #include <Arduino.h>
+#include <avr/power.h>
+
+#define SYSTEM_IO_PORT PORTA
+#define SYSTEM_IO_PIN PINA
+#define IS_OPEN_LED PA7
+#define IS_CLOSED_LED PA5
+#define TRIGGER_CHECK_PIN PINA4
 
 const int led = 0;
 const int sleep = 1000;
@@ -6,25 +13,42 @@ const int sleep = 1000;
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // initialize the digital pin as an output.
-  // bitSet(DDRB, led);
-  pinMode(PB0, OUTPUT);
-  digitalWrite(PB0, 0);
+    clock_prescale_set(clock_div_1);
+    // Set indicator outputs
+    bitSet(DDRA, IS_CLOSED_LED); 
+    bitSet(DDRA, IS_OPEN_LED);
+    bitClear(DDRA, TRIGGER_CHECK_PIN); // Read pulse state
 }
 
 // the loop routine runs over and over again forever
-void loop() {
-  pinMode(PB4, OUTPUT); // Configure PB4 as output
-  digitalWrite(PB4, 1); // Charge capacitor
-  delay(1); // wait for capacitor to charge
-
-  pinMode(PB4, INPUT); // Configure PB4 as input
-  bool isOpenCircuit = (digitalRead(PB4) == 1); // Read state of capacitor load
-  if (isOpenCircuit) {
-    digitalWrite(PB0, 1);
+void flashLeds(bool isOpen) {
+  if (isOpen) {
+    bitClear(SYSTEM_IO_PORT, IS_OPEN_LED);
+    bitSet(SYSTEM_IO_PORT, IS_CLOSED_LED);
   } else {
-    digitalWrite(PB0, 0);
+    bitClear(SYSTEM_IO_PORT, IS_CLOSED_LED);
+    bitSet(SYSTEM_IO_PORT, IS_OPEN_LED);
+  }
+}
+
+static bool ledsState = true;
+static bool canChangeState = true;
+
+void loop() {
+  /*
+    1. Move check to ISR
+    2. Implement state machine to ceck switch status
+    3. Add low power support 
+  */
+  unsigned long delayTime = 250;
+  if (bitRead(SYSTEM_IO_PIN, TRIGGER_CHECK_PIN)) {
+    delayTime = 1000;
+  } 
+
+  flashLeds(ledsState);
+  if (canChangeState) {
+    ledsState = !ledsState;
   }
 
-  delay(500);
+  delay(delayTime);
 }
