91736e68c2a506628b3fee51084e0c3b00de1ae7
B.E. Doron <b.e.doron@gmail.com>
2021-09-06 00:02:38 +0300

    Interrupt switch

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,21 @@
 #define IS_OPEN_LED PA7
 #define IS_CLOSED_LED PA5
 #define TRIGGER_CHECK_PIN PINA4
+#define TRIGGER_CHECK_PCINT PCINT4
+#define TRIGGER_CHECK_PCMSK PCMSK0 
 
-const int led = 0;
-const int sleep = 1000;
+#define DELAY_STEP 100
+#define MAX_DELAY 1100
 
+unsigned long delayTime = 250;
+
+ISR(PCINT0_vect) {
+  if (bitRead(SYSTEM_IO_PIN, TRIGGER_CHECK_PIN)) {
+    // If IDLE - Open Door + Start timer to shutdown
+    // If NOT IDLE - reset timer to shutdown
+    delayTime = (delayTime + DELAY_STEP) % MAX_DELAY;
+  }
+}
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -18,6 +29,12 @@ void setup() {
     bitSet(DDRA, IS_CLOSED_LED); 
     bitSet(DDRA, IS_OPEN_LED);
     bitClear(DDRA, TRIGGER_CHECK_PIN); // Read pulse state
+
+    // Enable interrupts
+    bitSet(GIMSK, PCIE0); // Enable Pin Change interrupts
+    GIFR = B00000000; // Clear state
+    bitSet(TRIGGER_CHECK_PCMSK, TRIGGER_CHECK_PCINT); // Enable pin specific interrupt
+    bitSet(SREG, SREG_I); // Enable interrupts
 }
 
 // the loop routine runs over and over again forever
@@ -32,23 +49,14 @@ void flashLeds(bool isOpen) {
 }
 
 static bool ledsState = true;
-static bool canChangeState = true;
 
 void loop() {
   /*
-    1. Move check to ISR
-    2. Implement state machine to ceck switch status
+    2. Implement state machine to check switch status
     3. Add low power support 
   */
-  unsigned long delayTime = 250;
-  if (bitRead(SYSTEM_IO_PIN, TRIGGER_CHECK_PIN)) {
-    delayTime = 1000;
-  } 
 
   flashLeds(ledsState);
-  if (canChangeState) {
-    ledsState = !ledsState;
-  }
-
+  ledsState = !ledsState;
   delay(delayTime);
 }
